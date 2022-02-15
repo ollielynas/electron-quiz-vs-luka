d9bd79d0ba0ea4f0c1d795a270c9efdf37f29934
ollielynas <lynasollie@gmail.com>
2022-02-15 23:11:28 +1300

    added check for max api calls

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -3,7 +3,12 @@
 /* eslint-disable no-console */
 
 import { useState, useEffect } from 'react';
-import { MemoryRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import {
+  MemoryRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+} from 'react-router-dom';
 import axios from 'axios';
 import quizData2 from './QuestionsJSON.json';
 import './App.css';
@@ -12,55 +17,28 @@ import './App.css';
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const quizData: { [key: string]: any } = quizData2;
 
+// eslint-disable-next-line promise/catch-or-return
 
 
-
-const data = {
-  Id: 10,
-  Name: "Jack Doe",
-  Age: 97,
-  "Created at": new Date(),
-};
-
-// Add one line to the sheet
-fetch("https://sheet.best/api/sheets/eeabed85-8ff0-449f-bcdc-8bcad89f5beb", {
-  method: "POST",
-  mode: "cors",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify(data),
-})
-  .then((r) => r.json())
-  // eslint-disable-next-line @typescript-eslint/no-shadow
-  .then((data) => {
-    // The response comes here
-    console.log(data);
-  })
-  .catch((error) => {
-    // Errors are reported there
-    console.log(error);
-  });
 
 const globalState = {
   score: 0,
   HScore: 0,
 };
 
-console.log("database",fetch(
-"https://sheet.best/api/sheets/eeabed85-8ff0-449f-bcdc-8bcad89f5beb"
-  ));
 
 console.log(quizData);
 
 // const { BrowserWindow } = require('electron');
 
 // const win = new BrowserWindow({ frame: false })
-  const styles = getComputedStyle(document.documentElement);
-  let animationDuration:number = parseInt(styles.getPropertyValue('--animation-duration'), 10);
+const styles = getComputedStyle(document.documentElement);
+let animationDuration: number = parseInt(
+  styles.getPropertyValue('--animation-duration'),
+  10
+);
 
 const Question = () => {
-
   const navigate = useNavigate();
   const [question, setQuestion] = useState('Question');
   const [reset, setReset] = useState(1);
@@ -71,39 +49,42 @@ const Question = () => {
     globalState.HScore = score;
   }
 
-  console.log("animation duration",animationDuration);
+  console.log('animation duration', animationDuration);
   const sleep = (milliseconds: number) => {
-    return new Promise(resolve => setTimeout(resolve, milliseconds))
-  }
+    return new Promise((resolve) => setTimeout(resolve, milliseconds));
+  };
 
-
-  const newQuestion = (ans:boolean | null) => {
-    setReset(reset+1);
+  const newQuestion = (ans: boolean | null) => {
+    setReset(reset + 1);
     console.log(ans);
     // eslint-disable-next-line prefer-template
-    document.documentElement.style.setProperty('--animation-duration', (animationDuration*0.9).toString() + 's');
+    document.documentElement.style.setProperty(
+      '--animation-duration',
+      `${(animationDuration * 0.9).toString()}s`
+    );
     animationDuration *= 0.95;
-    console.log("new question");
+    console.log('new question');
     const keys = Object.keys(quizData.active);
     console.log(keys);
     setQuestion(keys[Math.floor(Math.random() * keys.length)]);
-    if (ans!==null) {
-    if (quizData.active[question][1] === ans) {
-      setScore(score+1);
-    }else{setScore(score-1);};
-  }
-  }
+    if (ans !== null) {
+      if (quizData.active[question][1] === ans) {
+        setScore(score + 1);
+      } else {
+        setScore(score - 1);
+      }
+    }
+  };
 
   if (reset === 1) {
     newQuestion(null);
   }
 
   const gameTimer = async () => {
-    console.log('gameTimer1')
+    console.log('gameTimer1');
     sleep(10000);
     console.log('gameTimer');
-
-  }
+  };
 
   gameTimer();
 
@@ -114,65 +95,99 @@ const Question = () => {
         <p>{question}</p>
       </div>
       <div className="true-false-buttons">
-      <button type="button" className="Question-Body-Button" onClick={()=>{newQuestion(true);}}>True</button>
-      <button type="button" onClick={()=>{newQuestion(false);}} className="Question-Body-Button">False</button>
+        <button
+          type="button"
+          className="Question-Body-Button"
+          onClick={() => {
+            newQuestion(true);
+          }}
+        >
+          True
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            newQuestion(false);
+          }}
+          className="Question-Body-Button"
+        >
+          False
+        </button>
       </div>
-      <div className="progress-bar2"
-      key = {reset}
-      onAnimationEnd={() => {
-        newQuestion(null);}}
+      <div
+        className="progress-bar2"
+        key={reset}
+        onAnimationEnd={() => {
+          newQuestion(null);
+        }}
       />
-      <div className="progress-bar1"
-      onAnimationEnd={() => {navigate(`/`, { replace: true })}}
+      <div
+        className="progress-bar1"
+        onAnimationEnd={() => {
+          navigate(`/`, { replace: true });
+        }}
       />
     </div>
-  )
-}
+  );
+};
 
-
-const baseURL = "https://sheet.best/api/sheets/eeabed85-8ff0-449f-bcdc-8bcad89f5beb";
-
+const baseURL =
+  'https://sheet.best/api/sheets/eeabed85-8ff0-449f-bcdc-8bcad89f5beb';
 
 const HomePage = () => {
   const [title, SetTitle] = useState('Quiz !');
 
   const sleep = (milliseconds: number) => {
-    return new Promise(resolve => setTimeout(resolve, milliseconds))
-  }
+    return new Promise((resolve) => setTimeout(resolve, milliseconds));
+  };
 
   const navigate = useNavigate();
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  let databaseTable: any;
 
-  fetch(baseURL)
-  .then((response) => response.json())
-  .then((data2) => {
-    console.log(data2);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
 
   const updateLeaderboard = () => {
-  for (let i = 0; i < 20; i+= 1) {
-    if ()
-    fetch(`${baseURL}/${i.toString()}`, {
-      method: "PATCH",
-      mode: "cors",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        Name: "Jack Doe",
 
-      }),
+    let httpStatus = 0;
+    // eslint-disable-next-line promise/catch-or-return, func-names
+    fetch('https://sheet.best/api/sheets/eeabed85-8ff0-449f-bcdc-8bcad89f5beb').then(function(response) {
+  console.log("status",response.status);
+  httpStatus = response.status;
+});
+
+if (httpStatus === 200)  {
+
+    fetch(baseURL)
+    .then((response) => response.json())
+    .then((data2) => {
+      console.log(data2);
+      databaseTable = data2;
     })
-      .then((r) => r.json())
-      .then(console.log)
-      .catch(console.error);
-
-  }
-  }
-
+    .catch((error) => {
+      console.error(error);
+    });
+    console.log();
+    for (let i = 0; i < 20; i += 1) {
+      console.log(databaseTable[i+1]);
+      if (databaseTable[i].score > globalState.HScore) {
+        fetch(`${baseURL}/${i.toString()}`, {
+          method: 'PATCH',
+          mode: 'cors',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            Name: 'Jack Doe',
+            Score: 'globalState.HScore',
+          }),
+        })
+          .then((r) => r.json())
+          .then(console.log)
+          .catch(console.error);
+      }
+    }}
+  };
 
   const Bubbles = () => {
     return (
@@ -195,61 +210,72 @@ const HomePage = () => {
 
   return (
     <div className="App">
-      <button type="button" className="settings">&#x2699;</button>
+      <button type="button" className="settings">
+        &#x2699;
+      </button>
       <Bubbles />
       <div className="content">
         <div className="column">
-        <div className="title">
-          <p>{title}</p>
+          <div className="title">
+            <p>{title}</p>
+          </div>
+          <div className="buttons">
+            <button
+              type="button"
+              className="button"
+              onClick={async () => {
+                updateLeaderboard();
+                console.log('new game');
+                SetTitle('Ready.');
+                await sleep(850);
+                SetTitle('Ready..');
+                await sleep(800);
+                SetTitle('Ready...');
+                await sleep(750);
+                SetTitle('Go !');
+                await sleep(700);
+                document.documentElement.style.setProperty(
+                  '--animation-duration',
+                  '2s'
+                );
+                animationDuration = parseInt(
+                  styles.getPropertyValue('--animation-duration'),
+                  10
+                );
+                navigate(`/Question`, { replace: true });
+              }}
+            >
+              Play quiz &nbsp;{'>'}
+            </button>
+            <button className="button" type="button" onClick={async () => {}}>
+              Edit Question &nbsp;{'>'}
+            </button>
+            <button
+              className="button"
+              type="button"
+              onClick={() => {
+                console.log('owo');
+              }}
+            >
+              Import / Create Questions &nbsp;{'>'}
+            </button>
+          </div>
         </div>
-        <div className="buttons">
-        <button
-          type="button"
-          className="button"
-          onClick={async () => {
-            updateLeaderboard();
-            console.log("new game");
-            SetTitle('Ready.');
-            await sleep(850);
-            SetTitle('Ready..');
-            await sleep(800);
-            SetTitle('Ready...');
-            await sleep(750);
-            SetTitle('Go !');
-            await sleep(700);
-            document.documentElement.style.setProperty('--animation-duration', '2s');
-            animationDuration = parseInt(styles.getPropertyValue('--animation-duration'), 10);
-            navigate(`/Question`, { replace: true })
-          }}
-        >
-          Play quiz &nbsp;{'>'}
-        </button>
-        <button
-        className="button"
-          type="button"
-          onClick={async () => {
-
-          }}
-        >
-          Edit Question &nbsp;{'>'}
-        </button>
-        <button
-        className="button"
-          type="button"
-          onClick={() => {
-            console.log('owo');
-          }}
-        >
-          Import / Create Questions &nbsp;{'>'}
-        </button>
+        <div className="Last-Score">
+          <p>Last Score: {globalState.score}</p>
+          <p>Best Score: {globalState.HScore}</p>
+          <button
+            className="button"
+            type="button"
+            onClick={() => {
+              console.log('update leaderboard');
+              updateLeaderboard();
+            }}
+          >
+            Update
+          </button>
+        </div>
       </div>
-      </div>
-      <div className="Last-Score">
-        <p>Last Score: {globalState.score}</p>
-        <p>Best Score: {globalState.HScore}</p>
-      </div>
-      </div>
-
     </div>
   );
 };
